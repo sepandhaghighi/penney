e2af9b9c3199627c389b8d1a85eb99cc1bc85df8
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-15 02:01:33 +0330

    fix : setup files added

---
+++ setup.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""Setup module."""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+MINIMAL_DESCRIPTION = '''
+Penney's game, named after its inventor Walter Penney, is a binary (head/tail) sequence generating game between two or more players.
+Player A selects a sequence of heads and tails (of length 3 or larger), and shows this sequence to player B. Player B then
+selects another sequence of heads and tails of the same length. Subsequently, a fair coin is tossed until either player A's
+or player B's sequence appears as a consecutive subsequence of the coin toss outcomes. The player whose sequence appears first wins.
+'''
+
+
+def get_requires():
+    """Read requirements.txt."""
+    requirements = open("requirements.txt", "r").read()
+    return list(filter(lambda x: x != "", requirements.split()))
+
+
+def read_description():
+    """Read README.md and CHANGELOG.md."""
+    try:
+        with open("README.md") as r:
+            description = "\n"
+            description += r.read()
+        with open("CHANGELOG.md") as c:
+            description += "\n"
+            description += c.read()
+        return description
+    except Exception:
+        return MINIMAL_DESCRIPTION
+
+
+setup(
+    name='penney',
+    packages=['penney'],
+    version='0.1',
+    description="Penney's game",
+    long_description=read_description(),
+    long_description_content_type='text/markdown',
+    author='Sepand Haghighi',
+    author_email='info@4r7.ir',
+    url='https://github.com/sepandhaghighi/penney',
+    keywords="penney probability game",
+    project_urls={
+        'Source': 'https://github.com/sepandhaghighi/penney',
+        'Tracker': 'https://github.com/sepandhaghighi/penney/issues',
+    },
+    install_requires=get_requires(),
+    python_requires='>=2.7',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Fonts',
+        'Topic :: Text Editors',
+        'Topic :: Text Processing :: General',
+        'Topic :: Utilities',
+        'Topic :: Multimedia',
+        'Topic :: Printing',
+    ],
+    license='MIT',
+    include_package_data=True
+)

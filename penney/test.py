5ea49e2a777bd1b29c8f58fd2c5b92d0f6533d48
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-18 11:33:03 +0330

    fix : tests updated

--- penney/test.py
+++ penney/test.py
@@ -52,4 +52,57 @@ False
 False
 >>> check_seq(seq="HHHH",seq_len=4,seq_dict={1:"HTTT",2:"HHHH"})
 False
+>>> check_name("Name1",["Name2"])
+True
+>>> check_name("Name1",["Name1","Name2"])
+False
+>>> print_result(scores={"Player1":30,"Player2":32},seq_dict={"Player1":"HHT","Player2":"HTH"})
+Scores Table :
+Player2     32     HTH
+Player1     30     HHT
+Winner : Player2
+>>> random.seed(300)
+>>> computer_seq("HTH")
+'HHT'
+>>> player_filter(num=1,seq_len=3,print_status=True)
+[Warning] Number of players automatically set to 2
+2
+>>> player_filter(num=3,seq_len=3,print_status=True)
+3
+>>> player_filter(num=9,seq_len=3,print_status=True)
+[Warning] Number of players automatically set to 8
+8
+>>> description()
+ ____                                                             ____
+/\  _`\                                                          /\  _`\
+\ \ \L\ \   __     ___      ___       __    __  __               \ \ \L\_\     __       ___ ___       __
+ \ \ ,__/ /'__`\ /' _ `\  /' _ `\   /'__`\ /\ \/\ \               \ \ \L_L   /'__`\   /' __` __`\   /'__`\
+  \ \ \/ /\  __/ /\ \/\ \ /\ \/\ \ /\  __/ \ \ \_\ \               \ \ \/, \/\ \L\.\_ /\ \/\ \/\ \ /\  __/
+   \ \_\ \ \____\\ \_\ \_\\ \_\ \_\\ \____\ \/`____ \               \ \____/\ \__/.\_\\ \_\ \_\ \_\\ \____\
+    \/_/  \/____/ \/_/\/_/ \/_/\/_/ \/____/  `/___/> \               \/___/  \/__/\/_/ \/_/\/_/\/_/ \/____/
+                                                /\___/
+                                                \/__/
+<BLANKLINE>
+          ___      _
+__   __  / _ \    / |
+\ \ / / | | | |   | |
+ \ V /  | |_| | _ | |
+  \_/    \___/ (_)|_|
+<BLANKLINE>
+<BLANKLINE>
+####################################################################################################
+Penney's  game,  named after its inventor Walter Penney, is a binary (head/tail) sequence generating
+game  between  two  or  more players. Player A selects a sequence of heads and tails (of length 3 or
+larger),  and  shows  this sequence to player B. Player B then selects another sequence of heads and
+tails  of the same length. Subsequently, a fair coin is tossed until either player A's or player B's
+sequence  appears  as a consecutive subsequence of the coin toss outcomes. The player whose sequence
+appears first wins.
+####################################################################################################
+ __  __  _____  _   _  _   _
+|  \/  || ____|| \ | || | | |  _
+| |\/| ||  _|  |  \| || | | | (_)
+| |  | || |___ | |\  || |_| |  _
+|_|  |_||_____||_| \_| \___/  (_)
+<BLANKLINE>
+<BLANKLINE>
 """

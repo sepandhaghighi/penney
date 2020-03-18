48cdb43d82b2eb8f185fd89dcc6e00c34b918f99
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-18 19:14:07 +0330

    fix : build_exe.bat added

---
+++ build_exe.bat
@@ -0,0 +1,14 @@
+@echo off
+python --version | find /i "Python 3.4" > nul
+echo -----
+if errorlevel 1 (
+	echo Warning : Please use Python 3.4.x
+) else (
+	echo Python version check done!
+)
+echo -----
+python -m pip install –upgrade pip
+python -m pip install -r requirements.txt
+python -m pip install "PyInstaller>=3.3"
+python -m PyInstaller Penney.spec
+pause

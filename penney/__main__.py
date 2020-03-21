f824deb2a41727469bc2d624b4cab815979f4d97
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-21 19:44:50 +0430

    fix : minor edit in __main__.py

--- penney/__main__.py
+++ penney/__main__.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     while(True):
         description()
         menu()
-        control_input = input("Press [R] to restart or any other key to exit.")
+        control_input = input("\nPress [R] to restart or any other key to exit.")
         if control_input.upper() != "R":
             break

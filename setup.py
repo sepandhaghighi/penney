43b1ea8613578093115aafc620b9add0ddffcca8
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-04-13 21:06:44 +0430

    fix : minor edit in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=get_requires(),
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: End Users/Desktop',

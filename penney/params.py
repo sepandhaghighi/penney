09a9b7c05a631a40b39517bfe03ddde6d9a40d84
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-02-22 19:24:15 +0330

    fix : minor edit in SIMULATION_MODE_MESSAGE

--- penney/params.py
+++ penney/params.py
@@ -14,7 +14,7 @@ TRY_MESSAGE = "Try again!"
 
 PLAYER_COMPUTER_MESSAGE = "Play with computer [1]\nPlay with other players [any other key]\nPlease select : "
 
-SIMULATION_MODE_MESSAGE = "Slow simulation [1]\nFast simulation [any other key]\nPlease select : "
+SIMULATION_MODE_MESSAGE = "Fast simulation [1]\nSlow simulation [any other key]\nPlease select : "
 
 ROUND_NUMBER_MESSAGE = "Please enter number of rounds : "
 

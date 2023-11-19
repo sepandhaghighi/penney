50a4edc009dfed68cb4e0a75dae37109517589b7
sepandhaghighi <sepand.haghighi@yahoo.com>
2023-11-19 23:45:37 +0330

    fix : version_check.py script updated

--- otherfiles/version_check.py
+++ otherfiles/version_check.py
@@ -15,6 +15,7 @@ SETUP_ITEMS = ["version='{0}'"]
 README_ITEMS = [
     "[Version {0}](https://github.com/sepandhaghighi/penney/archive/v{0}.zip)",
     "Run `Penney-{0}.exe`",
+    "Run `pip install penney=={0}`",
     "[Exe-Version {0}](https://github.com/sepandhaghighi/penney/releases/download/v{0}/Penney-{0}.exe)"]
 CHANGELOG_ITEMS = [
     "## [{0}]",
